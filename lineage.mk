04adde59bb47b38024544cf5aadb3895c3e3c282
Anik1199 <anik@lineageos.org>
2017-08-18 18:51:30 +0600

    ido: Change pathmaps to lineage
    Change-Id: If735ca43c7c993d6d73d825ad3e2eadb0572ae09

--- lineage.mk
+++ lineage.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, device/xiaomi/ido/full_ido.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_ido
 BOARD_VENDOR := Xiaomi
