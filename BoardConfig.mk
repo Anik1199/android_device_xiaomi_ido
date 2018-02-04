aff1bc8ef925c263d818b23b7e123261941bdfa5
Anik1199 <anik@lineageos.org>
2018-02-04 07:52:07 +0100

    ido: Include legacy QCOM sepolicy
    Change-Id: I84fbc87031f8a80f88f4a89148c434f4ee9dcf07

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -168,6 +168,8 @@ TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
+
 BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
 
 # Wifi
