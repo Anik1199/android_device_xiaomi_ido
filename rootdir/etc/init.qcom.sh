81a5610f896e61520a310a40cf445f413d3e5723
Anik1199 <anik@lineageos.org>
2018-03-08 08:28:01 +0000

    ido: Move init shell scripts to /vendor

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -1,4 +1,4 @@
-#!/system/bin/sh
+#!/vendor/bin/sh
 # Copyright (c) 2009-2015, The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -26,7 +26,7 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-export PATH=/system/bin
+export PATH=/vendor/bin
 
 # Set platform variables
 target=`getprop ro.board.platform`
